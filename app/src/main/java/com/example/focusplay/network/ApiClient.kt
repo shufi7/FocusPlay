674e5f04c498ce39d788fd30f9c5d667bb3e17cf
shufi7 <johanvetran@gamil.com>
2026-05-15 20:57:57 +0800

    Mengubah BASE_URL menggunakan IP Wi-Fi untuk testing di HP

--- app/src/main/java/com/example/focusplay/network/ApiClient.kt
+++ app/src/main/java/com/example/focusplay/network/ApiClient.kt
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiClient {
     // PENTING: Gunakan 10.0.2.2 untuk mengakses localhost XAMPP dari Emulator Android
-    private const val BASE_URL = "http://10.0.2.2:8000/"
+    private const val BASE_URL = "http://10.38.247.145:8000/"
 
     val instance: ApiService by lazy {
         val retrofit = Retrofit.Builder()
